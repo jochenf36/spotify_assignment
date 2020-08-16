41afdf3f4563cc0948d8c0bdc4b554d2d17065e1
jochen <jochen.francois@kbc.be>
2020-08-16 17:13:43 +0200

    Updates

---
+++ precache-manifest.74f43011b7ec364cec7390653aebe876.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041fdf385bf28139051e9925f1805180",
+    "url": "/spotify_assignment/index.html"
+  },
+  {
+    "revision": "5bad064d02c36b76d3c4",
+    "url": "/spotify_assignment/static/css/main.1844fc94.chunk.css"
+  },
+  {
+    "revision": "c296ac88a4329908b3f4",
+    "url": "/spotify_assignment/static/js/2.59d30fe8.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/spotify_assignment/static/js/2.59d30fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bad064d02c36b76d3c4",
+    "url": "/spotify_assignment/static/js/main.807502be.chunk.js"
+  },
+  {
+    "revision": "57b124c890b371c8e57f",
+    "url": "/spotify_assignment/static/js/runtime-main.7d206945.js"
+  },
+  {
+    "revision": "d86931f419fb3d95f67298c53679f02f",
+    "url": "/spotify_assignment/static/media/rings.d86931f4.svg"
+  }
+]);
